6c51e1eb1aa3bc1abb9d99fb0b86075b6865245c
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-04-13 07:28:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
-arr.sort();
-console.log(arr);
+let updatedList = arr.map((element) => {
+	// ["a", "some", "one"]
+	let words = element.split(" ") ;
+	let updatedString = words.reduce((prev, current, index) => {
+		if(current != "The" && current != "an" && current != "a"){
+			return prev + " " + current ;
+		}
+		return prev ;
+	}, "")
+
+	return updatedString ;
+}) ;
+let mp = {} ; // {"some one" : "a some one"}
+updatedList.forEach( (element, index) => {
+	mp[element] = arr[index] ;
+});
+updatedList.sort(); 
+
+let finalAns = updatedList.map((element) => {
+    return mp[element] ;
+})
+
+console.log(finalAns); 
